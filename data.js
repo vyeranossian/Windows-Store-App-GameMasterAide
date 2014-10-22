cf60ac5f24b3f2eb2363612a470b8bac17c00124
Vahagn Yeranossian <vyeranossian@gmail.com>
2014-10-22 08:17:17 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,83 @@
+// This Source Code Form is subject to the terms of 
+// the Mozilla Public License, v. 2.0. 
+// If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+(function () {
+    "use strict";
+
+    var list = new WinJS.Binding.List();
+    var groupedItems = list.createGrouped(
+        function groupKeySelector(item) { return item.group.key; },
+        function groupDataSelector(item) { return item.group; }
+    );
+
+    // TODO: Replace the data with your real data.
+    // You can add data from asynchronous sources whenever it becomes available.
+    generateSampleData().forEach(function (item) {
+        list.push(item);
+    });
+
+    WinJS.Namespace.define("Data", {
+        items: groupedItems,
+        groups: groupedItems.groups,
+        getItemReference: getItemReference,
+        getItemsFromGroup: getItemsFromGroup,
+        resolveGroupReference: resolveGroupReference,
+        resolveItemReference: resolveItemReference
+    });
+
+    // Get a reference for an item, using the group key and item title as a
+    // unique reference to the item that can be easily serialized.
+    function getItemReference(item) {
+        return [item.group.key, item.title];
+    }
+
+    // This function returns a WinJS.Binding.List containing only the items
+    // that belong to the provided group.
+    function getItemsFromGroup(group) {
+        return list.createFiltered(function (item) { return item.group.key === group.key; });
+    }
+
+    // Get the unique group corresponding to the provided group key.
+    function resolveGroupReference(key) {
+        return groupedItems.groups.getItemFromKey(key).data;
+    }
+
+    // Get a unique item from the provided string array, which should contain a
+    // group key and an item title.
+    function resolveItemReference(reference) {
+        for (var i = 0; i < groupedItems.length; i++) {
+            var item = groupedItems.getAt(i);
+            if (item.group.key === reference[0] && item.title === reference[1]) {
+                return item;
+            }
+        }
+    }
+
+    // Returns an array of sample data that can be added to the application's
+    // data list. 
+    function generateSampleData() {
+        var itemContent = "This is content";
+        var itemDescription = "Item Description: This is a description";
+        var groupDescription = "Select the specific application that you desire";
+
+        // These three strings encode placeholder images. You will want to set the
+        // backgroundImage property in your real data to be URLs to images.
+        var darkGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY3B0cPoPAANMAcOba1BlAAAAAElFTkSuQmCC";
+        var lightGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY7h4+cp/AAhpA3h+ANDKAAAAAElFTkSuQmCC";
+        var mediumGray = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXY5g8dcZ/AAY/AsAlWFQ+AAAAAElFTkSuQmCC";
+
+        // Each of these sample groups must have a unique key to be displayed
+        // separately.
+        var itemGroup = { key: "itemgroup", title: "GMAide Option Selection", subtitle: "placeholder subtitle", backgroundImage: darkGray, description: groupDescription };
+
+        // Each of these sample items should have a reference to a particular
+        // group.
+
+        var GMAideItems = [
+            { group: itemGroup, title: "Dungeon Maker", subtitle: "Item Subtitle: 1", description: "Randomly generate and populate a dungeon", content: itemContent, backgroundImage: darkGray },
+            { group: itemGroup, title: "Description Generator", subtitle: "Item Subtitle: 2", description: "Randomly generate a description for a room or NPC", content: itemContent, backgroundImage: lightGray },
+        ]
+        return GMAideItems;
+    }
+})();
